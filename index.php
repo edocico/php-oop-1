4e3bd4ca0bff24f16c6c9b68b9d1867c4e0bfc09
edocico <138378767+edocico@users.noreply.github.com>
2023-11-28 17:14:50 +0100

    printed istances in page

--- index.php
+++ index.php
@@ -29,4 +29,39 @@ var_dump($production2);
 $production3 = new Production('Full metal jacket', 'English', 9.5, 'Kubrick');
 var_dump($production3);
 
+$film_list = [
+    $production1,
+    $production2,
+    $production3,
+];
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <h1>Tabella dei Film</h1>
+    </header>
+    <main>
+        <ul>
+            <?php foreach ($film_list as $production) { ?>
+                <li>TITOLO: <?php echo $production->title?></li>
+                <li>LINGUA ORIGINALE: <?php echo $production->language?></li>
+                <li>VOTO: <?php echo $production->rating?></li>
+                <li>REGISTA: <?php echo $production->director?></li>
+            <?php
+            }
+            ?>
+        </ul>
+    </main>
+    
+</body>
+</html>
